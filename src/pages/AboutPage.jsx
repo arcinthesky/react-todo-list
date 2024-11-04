4ef825f3901d87db881c887591184c965f3f7345
arcinthesky <r.santapol@gmail.com>
2024-11-05 04:13:15 +0700

    First Commit

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,17 @@
+const AboutPage = () => {
+  return (
+    <div className="p-6 bg-white rounded-md shadow-sm">
+      <h1 className="font-bold text-2xl mb-3">About</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor fugit
+        veniam eveniet. Ex et, neque omnis laudantium saepe nostrum dignissimos
+        assumenda ducimus natus fugit culpa inventore optio beatae nesciunt.
+        Porro provident voluptatibus, aliquid suscipit voluptas quos itaque?
+        Modi ad, porro laboriosam iure earum ducimus ipsam quaerat, dolorum ab
+        sapiente voluptatibus.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
